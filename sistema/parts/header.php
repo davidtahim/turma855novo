11d1c91021be873ea0d4ce2290d6dafef2aa2e18
David Tahim <davidtahim@gmail.com>
2023-10-04 13:22:27 -0300

    sistema do petshop

---
+++ sistema/parts/header.php
@@ -0,0 +1,21 @@
+<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="Sistema para a gestão administrativa do petshop">
+	<meta name="author" content="David Tahim">
+	<meta name="keywords" content="pet, gestão, petshop, dashboard">
+
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link rel="shortcut icon" href="img/icons/icon-48x48.png"/>
+	<link rel="shortcut icon" href="img/icons/favicon.ico" type="image/x-icon">
+
+	<link rel="canonical" href="https://demo-basic.adminkit.io/" />
+
+	<title>Sistema PetShop</title>
+
+	<link href="css/app.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/principal.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
+	<script src="https://kit.fontawesome.com/a1f9544770.js" crossorigin="anonymous"></script>
+</head>
